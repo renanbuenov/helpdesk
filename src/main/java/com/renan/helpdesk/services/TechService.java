9c3041bc12705d8d309a5d973cba8fc716099506
renanbuenov <renanbuenov@hotmail.com>
2021-10-23 16:26:53 -0300

    Exception for object not found created

--- src/main/java/com/renan/helpdesk/services/TechService.java
+++ src/main/java/com/renan/helpdesk/services/TechService.java
@@ -2,6 +2,7 @@ package com.renan.helpdesk.services;
 
 import java.util.Optional;
 
+import com.renan.helpdesk.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +17,6 @@ public class TechService {
 	
 	public Technician findById(Integer id) {
 		Optional<Technician> obj = repository.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found! Id: " + id));
 	}
 }
