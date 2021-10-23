e333707202ca11bbd248a4db447ef751ec871a1d
renanbuenov <renanbuenov@hotmail.com>
2021-10-23 15:59:17 -0300

    DTO pattern for Technician

--- src/main/java/com/renan/helpdesk/domain/dtos/TechnicianDTO.java
+++ src/main/java/com/renan/helpdesk/domain/dtos/TechnicianDTO.java
@@ -1,118 +1,96 @@
-package com.renan.helpdesk.domain;
+package com.renan.helpdesk.domain.dtos;
 
 import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.HashSet;
-import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-import javax.persistence.CollectionTable;
-import javax.persistence.Column;
-import javax.persistence.ElementCollection;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-
-import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.renan.helpdesk.domain.Technician;
 import com.renan.helpdesk.domain.enums.Profile;
 
-@Entity
-public abstract class Person implements Serializable {
+public class TechnicianDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	protected Integer id;
 	protected String name;
-	
-	@Column(unique = true)
-	protected String cpf;
-	
-	@Column(unique = true)
+	protected String cpf;	
 	protected String email;
-	protected String password;
-	
-	@ElementCollection(fetch = FetchType.EAGER)
-	@CollectionTable(name = "PROFILE")
+	protected String password;	
 	protected Set<Integer> profiles = new HashSet<>();
 	
 	@JsonFormat(pattern = "dd/MM/yyyy")
 	protected LocalDate creationDate = LocalDate.now();
-	
-	public Person() {
-		super();
-		addProfile(Profile.CLIENT);
-	}
-	public Person(Integer id, String name, String cpf, String email, String password) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.cpf = cpf;
-		this.email = email;
-		this.password = password;
-		addProfile(Profile.CLIENT);
 
+	public TechnicianDTO() {
+		super();
 	}
+
+	public TechnicianDTO(Technician obj) {
+		super();
+		this.id = obj.getId();
+		this.name = obj.getName();
+		this.cpf = obj.getCpf();
+		this.email = obj.getEmail();
+		this.password = obj.getPassword();
+		this.profiles = obj.getProfiles().stream().map(x -> x.getCode()).collect(Collectors.toSet());
+		this.creationDate = obj.getCreationDate();
+	}
+
 	public Integer getId() {
 		return id;
 	}
+
 	public void setId(Integer id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getCpf() {
 		return cpf;
 	}
+
 	public void setCpf(String cpf) {
 		this.cpf = cpf;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
 	public Set<Profile> getProfiles() {
 		return profiles.stream().map(x -> Profile.toEnum(x)).collect(Collectors.toSet());
 	}
+
 	public void addProfile(Profile profile) {
 		this.profiles.add(profile.getCode());
-	}
+;	}
+
 	public LocalDate getCreationDate() {
 		return creationDate;
 	}
+
 	public void setCreationDate(LocalDate creationDate) {
 		this.creationDate = creationDate;
-	}
-	
-	@Override
-	public int hashCode() {
-		return Objects.hash(cpf, id);
-	}
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Person other = (Person) obj;
-		return Objects.equals(cpf, other.cpf) && Objects.equals(id, other.id);
 	}
 }
--- src/main/java/com/renan/helpdesk/resources/TechResource.java
+++ src/main/java/com/renan/helpdesk/resources/TechResource.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.renan.helpdesk.domain.Technician;
+import com.renan.helpdesk.domain.dtos.TechnicianDTO;
 import com.renan.helpdesk.services.TechService;
 
 @RestController
@@ -18,9 +19,9 @@ public class TechResource {
 	private TechService service;
 	
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<Technician> findById(@PathVariable Integer id){
+	public ResponseEntity<TechnicianDTO> findById(@PathVariable Integer id){
 		Technician obj = service.findById(id);
-		return ResponseEntity.ok().body(obj);
+		return ResponseEntity.ok().body(new TechnicianDTO(obj));
 	}
 
 }
