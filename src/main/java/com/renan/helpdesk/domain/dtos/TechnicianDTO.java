0c611e6cb78cb5a05f32a4669b9b4571cfb69a10
renanbuenov <renanbuenov@hotmail.com>
2021-10-23 17:54:05 -0300

    Fixed profiles list bug

--- src/main/java/com/renan/helpdesk/domain/dtos/TechnicianDTO.java
+++ src/main/java/com/renan/helpdesk/domain/dtos/TechnicianDTO.java
@@ -37,6 +37,7 @@ public class TechnicianDTO implements Serializable {
 		this.password = obj.getPassword();
 		this.profiles = obj.getProfiles().stream().map(x -> x.getCode()).collect(Collectors.toSet());
 		this.creationDate = obj.getCreationDate();
+		addProfile(Profile.CLIENT);
 	}
 
 	public Integer getId() {
